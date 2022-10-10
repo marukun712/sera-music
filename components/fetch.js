575a2d03034b1883bb3bf7837b99768258bcc8eb
marukun_ <87289638+marukun712@users.noreply.github.com>
2022-10-10 20:43:41 +0900

    deploy vercel

--- components/fetch.js
+++ components/fetch.js
@@ -8,7 +8,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 const Home = () => {
   Comment.getid();
   const { data, error } = useSWR(
-    "https://sera-music.vercel.app/",
+    "https://sera-music.vercel.app/api/api",
     fetcher
   );
 
